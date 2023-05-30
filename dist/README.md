871afd8981d2189387b95ff454ae6650660eab6f
lersin <skydirge@gmail.com>
2023-05-30 07:39:38 +0000

    Modify frr controller rbac and deployment yaml

--- dist/README.md
+++ dist/README.md
@@ -1,23 +1,21 @@
 # frr-controller
 
-## build frr controller
-```sh
-cd frr-controller
-go mod vendor
-go build -o frr-controller .
-```
-
 ## copy
 ```sh
 cd frr-controller
 cp ./frr-controller ./dist/images/frr-controller
-cp /root/.kube/config ./dist/images/config
+```
+
+## build git info
+```sh
+cd frr-controller/dist/images
+./build_git_info.sh
 ```
 
 ## build image
 ```sh
 cd frr-controller/dist/images
-docker build -t nocsys/frr:0.1-saic -f Dockerfile .
+docker build -t nocsyscn/frr:0.1-saic -f Dockerfile .
 ```
 
 ## run frr controller
--- dist/images/frr.sh
+++ dist/images/frr.sh
@@ -2,64 +2,31 @@
 #set -euo pipefail
 
 # The argument to the command is the operation to be performed
-# ovn-master ovn-controller ovn-node display display_env ovn_debug
 # a cmd must be provided, there is no default
 cmd=${1:-""}
 
-kubeconfig=$HOME/.kube/config
-
-ovn_pod_host=${K8S_NODE:-$(hostname)}
-ovn_daemonset_version=${OVN_DAEMONSET_VERSION:-"3"}
-ovnkube_version="3"
-ovnkubelogdir=/var/log/ovn-kubernetes
-
-if [[ -f /var/run/secrets/kubernetes.io/serviceaccount/token ]]; then
-  k8s_token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-else
-  k8s_token=${K8S_TOKEN}
-fi
-
-# certs and private keys for k8s
-K8S_CACERT=${K8S_CACERT:-/var/run/secrets/kubernetes.io/serviceaccount/ca.crt}
-
-# =========================================
+frr_pod_name=${FRR_POD_NAME:-$(hostname)}
+frrlogdir=/var/log/frr-controller
 
 display_version() {
-  echo " =================== hostname: ${ovn_pod_host}"
-  echo " =================== daemonset version ${ovn_daemonset_version}"
+  echo " =================== Frr pod name: ${frr_pod_name}"
   if [[ -f /root/git_info ]]; then
     disp_ver=$(cat /root/git_info)
-    echo " =================== Image built from ovn-kubernetes ${disp_ver}"
+    echo " =================== Image built from frr-controller ${disp_ver}"
     return
   fi
 }
 
-check_ovn_daemonset_version() {
-  ok=$1
-  for v in ${ok}; do
-    if [[ $v == ${ovn_daemonset_version} ]]; then
-      return 0
-    fi
-  done
-  echo "VERSION MISMATCH expect ${ok}, daemonset is version ${ovn_daemonset_version}"
-  exit 1
-}
-
-# v3 - run frr controller
+# run frr controller
 frr-controller() {
-  check_ovn_daemonset_version "3"
-
-  echo "=============== frr-controller ========== MASTER ONLY"
+  echo "=============== frr-controller =============== "
   /usr/bin/frr-controller \
-    -log_dir=${ovnkubelogdir} \
-    -log_file=frr-controller.log \
-    -kubeconfig=${kubeconfig}
+    -log_dir=${frrlogdir} \
+    -log_file=${frrlogdir}/frr-controller.log
 
   echo "=============== frr-controller ========== running"
 }
 
-
-echo "================== frr.sh --- version: ${ovnkube_version} ================"
 
 echo " ==================== command: ${cmd}"
 display_version
